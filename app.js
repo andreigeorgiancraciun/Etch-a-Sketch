5d7ce73d10bb9e65d81f77679a4d2e9a32b1c3bf
Andrei Craciun <andreigeorgiancraciun@yahoo.com>
2019-07-17 12:52:16 +0300

    form 2.0

---
+++ app.js
@@ -0,0 +1,84 @@
+const randomButton = document.getElementById("random-btn");
+const resetButton = document.getElementById("reset-btn");
+const blackButton = document.getElementById("black-btn");
+const cleanButton = document.getElementById("clear-btn");
+
+createGrid(16);
+
+function createGrid(gridSize){
+
+    let gridContainer = document.querySelector('.grid-container');   
+
+    gridContainer.style.gridTemplateColumns = "1fr ".repeat(gridSize);
+	gridContainer.style.gridTemplateRows = "1fr ".repeat(gridSize);
+
+    for (let j = 0; j < (gridSize * gridSize); j++) {
+        var gridCell = document.createElement("div");
+        gridCell.classList.add('grid-cell');
+        gridContainer.appendChild(gridCell);
+    }
+
+    let gridCells = document.getElementsByClassName('grid-cell');
+	let gridCellsArr = Array.from(gridCells);
+
+    gridCellsArr.forEach(function (cell) {
+        cell.addEventListener("mouseover", function(){
+            cell.classList.add('painted');
+        });
+        
+        cell.addEventListener("click", function(){
+            clearCell(cell);
+        });
+    });
+
+}
+
+cleanButton.addEventListener("click", function(){
+    let gridCells = document.getElementsByClassName('grid-cell');
+        
+    for (let j = 0; j < gridCells.length; j++) {
+		clearCell(gridCells[j]);
+	}
+		
+});
+
+randomButton.addEventListener("click", function () {
+	randomizeColor();
+});
+
+blackButton.addEventListener("click", function () {
+	blackColor();
+});
+
+function clearCell(cell) {
+    cell.classList.remove("painted");
+    cell.style.backgroundColor = "white";
+}
+
+function blackColor() {
+	gridCells = document.getElementsByClassName('grid-cell');
+	gridCellsArr = Array.from(gridCells);
+	gridCellsArr.forEach(function (cell) {
+		cell.addEventListener("mouseover", function () {
+			cell.classList.add("painted");
+		});
+	});
+}
+
+function randomizeColor() {
+	gridCells = document.getElementsByClassName('grid-cell');
+	gridCellsArr = Array.from(gridCells);
+	gridCellsArr.forEach(function (cell) {
+		cell.addEventListener("mouseover", function () {
+			cell.classList.remove("painted");
+			cell.style.backgroundColor = getRandom();
+		});
+	});
+}
+
+function getRandom(){
+    var r = Math.floor(Math.random() * 255);
+    var g = Math.floor(Math.random() * 255);
+    var b = Math.floor(Math.random() * 255);
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
