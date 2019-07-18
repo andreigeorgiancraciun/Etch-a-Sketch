25a6facb3aaa00ec4cacf63bfea9335146ac5cfc
Andrei Craciun <andreigeorgiancraciun@yahoo.com>
2019-07-18 16:05:22 +0300

    ver 4.0

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const resetButton = document.getElementById("reset-btn");
 const blackButton = document.getElementById("black-btn");
 const cleanButton = document.getElementById("clear-btn");
 
+
 createGrid(16);
 
 function createGrid(gridSize){
 
     let gridContainer = document.querySelector('.grid-container');   
-
+    
+    while (gridContainer.firstChild) {
+		gridContainer.firstChild.remove();
+    }
+    
     gridContainer.style.gridTemplateColumns = "1fr ".repeat(gridSize);
 	gridContainer.style.gridTemplateRows = "1fr ".repeat(gridSize);
 
@@ -49,6 +54,26 @@ randomButton.addEventListener("click", function () {
 blackButton.addEventListener("click", function () {
 	blackColor();
 });
+
+resetButton.addEventListener('click', first);
+
+function first(e){
+    resetButton.innerHTML = 'Submit';
+    document.getElementById("hide").style.display = "inline";
+    e.stopImmediatePropagation();
+    this.removeEventListener("click", first);
+    resetButton.onclick = second; 
+}
+  
+function second(){
+    let nr = document.getElementById("nr").value;
+    createGrid(nr);
+    document.getElementById("hide").style.display = "none";
+    resetButton.innerHTML = "Change Table Nr.";
+    resetButton.onclick = first; 
+}
+
+
 
 function clearCell(cell) {
     cell.classList.remove("painted");
